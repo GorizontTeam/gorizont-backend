2b66ae0c5ad887903592df5037dfe55821105ae0
Raynur <raynur.khasanov@gmail.com>
2022-03-07 11:17:44 +0300

    update STATIC and MEDIA settings

--- config/settings.py
+++ config/settings.py
@@ -139,6 +139,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'public/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static', ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'public/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
