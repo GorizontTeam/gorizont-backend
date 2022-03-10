f00cf32a43eb88b022490e8f5588f3cab241b8ec
Raynur <raynur.khasanov@gmail.com>
2022-03-10 16:58:50 +0300

    add apps to project

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-)0e)3%&=1s@9o9@c0@kuxd9d7n*pj)1*nlbrs^ano*y02je!5(
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -42,6 +40,13 @@ INSTALLED_APPS = [
     'graphene_django',
 
     'accounts',
+    'gamification',
+    'mentor',
+    'courses',
+    'cross_check',
+    'events_сalendar',
+    'media_library',
+    'notifications',
 ]
 
 MIDDLEWARE = [
@@ -90,7 +95,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -100,7 +104,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -120,7 +123,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -133,7 +135,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
---
+++ events_сalendar/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EventsСalendarConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'events_сalendar'
