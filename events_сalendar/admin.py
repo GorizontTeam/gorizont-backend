a0c089901dc89a654629d2dd36ffb8886be4077d
gubae <gubaewa.alsu@yandex.ru>
2022-04-18 19:47:21 +0300

    Alsu added new files

--- events_сalendar/admin.py
+++ events_сalendar/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from events_сalendar.models import Event
+from events_сalendar.models import Event, Status
 
 # Register your models here.
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Status)
+class StatusAdmin(admin.ModelAdmin):
     pass
--- events_сalendar/models.py
+++ events_сalendar/models.py
@@ -1,12 +1,26 @@
 from django.db import models
 from accounts.models import User
 
+
 # Create your models here.
+class Status(models.Model):
+    event_status = models.CharField('Статус события', max_length=150)
+
+    class Meta:
+        verbose_name = 'Статус'
+        verbose_name_plural = 'Статусы'
+
+    def __str__(self):
+        return self.event_status
+
+
 
 class Event(models.Model):
     name = models.CharField('Название', max_length=150)
-    date = models.DateTimeField('Дата и время')
+    start_date = models.DateTimeField('Дата и время начала')
+    finish_date = models.DateTimeField('Дата и время окончания', null=True, blank=True)
     author = models.ForeignKey(User, verbose_name='Автор', on_delete=models.CASCADE)
+    status = models.ForeignKey(Status, verbose_name='Статус', on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = 'Событие'
@@ -14,4 +28,6 @@ class Event(models.Model):
 
     def __str__(self):
         return self.name
+
+
 
