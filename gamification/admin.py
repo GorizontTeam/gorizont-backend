8c09db6a5d4b5beccb8f1a1d574f5c36d6dbf5f5
gubae <gubaewa.alsu@yandex.ru>
2022-04-20 21:44:27 +0300

    Added Achievement model. Created AchievementDot.

--- gamification/admin.py
+++ gamification/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from gamification.models import Achievement
 
 # Register your models here.
+@admin.register(Achievement)
+class AchievementAdmin(admin.ModelAdmin):
+    pass
