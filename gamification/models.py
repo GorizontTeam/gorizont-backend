18e86a22815a98cfe8d560db4de3b92435ffe304
gubae <gubaewa.alsu@yandex.ru>
2022-05-28 15:26:29 +0300

    Added AchievementUser model.

--- gamification/models.py
+++ gamification/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from accounts.models import User
+
 
 # Create your models here.
 
 
 class Achievement(models.Model):
-    img = models.ImageField('Файл', upload_to='achievements_images', height_field=None, width_field=None, max_length=100)
+    img = models.ImageField('Файл', upload_to='achievements_images', height_field=None, width_field=None,
+                            max_length=100)
     name = models.CharField('Название', max_length=150)
     description = models.CharField('Описание', max_length=100)
 
@@ -14,3 +17,15 @@ class Achievement(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class AchievementUser(models.Model):
+    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
+    achivement = models.ForeignKey(Achievement, verbose_name='Достижение', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Достигнутое достижение'
+        verbose_name_plural = 'Достигнутые достиженияя'
+
+    def __str__(self):
+        return self.user
