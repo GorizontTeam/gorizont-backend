1124d18400907eb062937449fed84817c3e89699
Raynur <raynur.khasanov@gmail.com>
2022-06-04 12:07:16 +0300

    edit alsu's code

--- gamification/simple_recomendation.py
+++ gamification/simple_recomendation.py
@@ -1,5 +1,6 @@
 # from __future__ import print_function
 import django
+
 django.setup()
 import locale
 import sys
@@ -11,15 +12,20 @@ from gamification.models import UserAchievement
 sys.path.append("src/")
 locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 
-def jaccard_index(set1,set2):
-  set1    = set(set1)
-  set2    = set(set2)
-  intrsct = float(len(set1.intersection(set2)))
-  union   = float(len(set1.union(set2)))
-  jaccard_index = intrsct/union
-  return round(jaccard_index,3)
 
-#Считает топ 15 схожих пользователей для авторизованного пользователя
+def jaccard_index(set1, set2):
+    set1 = set(set1)
+    set2 = set(set2)
+    intrsct = float(len(set1.intersection(set2)))
+    # print('intrsct', intrsct)
+    union = float(len(set1.union(set2)))
+    # print('union', union)
+    jaccard_index = intrsct / union
+    # print('jaccard_index', jaccard_index)
+    return round(jaccard_index, 3)
+
+
+# Считает топ 15 схожих пользователей для авторизованного пользователя
 def get_top_N_similar(username, preferences, N):
     user_preference = preferences.filter(user__username=username)
     if user_preference:
@@ -38,11 +44,12 @@ def get_top_N_similar(username, preferences, N):
                 similar.append(
                     (jaccard_index(user_preference, current_iteration_user_preference), preference.user),
                 )
-        print(similar)
-        similar.sort(reverse=True)
+        # print(similar)
+        similar.sort(key=lambda tup: tup[0], reverse=True)
         return similar[:N]
 
-#Считает количество пользователей, которые получили выбранное достижение
+
+# Считает количество пользователей, которые получили выбранное достижение
 def occurs_in(post_id, preferences, users):
     count = 0
     for user in users:
@@ -51,30 +58,57 @@ def occurs_in(post_id, preferences, users):
             count += 1
     return count
 
+
 def get_achievements_from_user_achievements(preferences):
-    return preferences.value_list('achievement', flat=True)
+    return preferences.values_list('achievement', flat=True)
 
 
 # uncomment to make give_recommendations work!
 def give_achievement_recommendations(username):
     N = 15  # количество пользователей в топе
     preferences = UserAchievement.objects.all()
-    preference = preferences.filter(user__username=username)  # достигнутые достижения для выбранного(авторизованного) пользователя
-    top_similar = get_top_N_similar(username, preferences, N)  # 15 пользователей с наибольшим сходством Жаккарда. На выходе получаем список пар сходство-пользователь
-    similar_users = [user for snimilarity, user in top_similar]  # 15 пользователей с наибольшим сходством Жаккарда. На выходе получаем список пользователей(только пользователей)
-    rank = {} # словарь коэффициентов сходства по достижениям
-    for similarity, other_user in top_similar: # по схожести и другим пользователям из списка 15 топов проходимся
-        other_preferences = preferences.filter(user=other_user) # список достигнутых достижений выбранного пользователя из списка 15 топов
+    preference = preferences.filter(
+        user__username=username)  # достигнутые достижения для выбранного(авторизованного) пользователя
+    top_similar = get_top_N_similar(username, preferences,
+                                    N)  # 15 пользователей с наибольшим сходством Жаккарда. На выходе получаем список пар сходство-пользователь
+    similar_users = [user for snimilarity, user in
+                     top_similar]  # 15 пользователей с наибольшим сходством Жаккарда. На выходе получаем список пользователей(только пользователей)
+    rank = {}  # словарь коэффициентов сходства по достижениям
+    for similarity, other_user in top_similar:  # по схожести и другим пользователям из списка 15 топов проходимся
+        other_preferences = preferences.filter(
+            user=other_user)  # список достигнутых достижений выбранного пользователя из списка 15 топов
         current_user_achievement = get_achievements_from_user_achievements(preference)
         other_user_achievements = get_achievements_from_user_achievements(other_preferences)
-        for post_id in other_user_achievements: # для id достижения пользователя из other_preferences
-            if post_id not in current_user_achievement: # проверяем достигнуто ли достижение авторизованным пользователем
-                rank.setdefault(post_id, 0) # создаём пустую запись для достижения в ранк, если она не была создана раньше
-                rank[post_id] += similarity # прибавляем коэффициент сходства для достижения
-    recommendations = [(similarity / occurs_in(post_id, preferences, similar_users), post_id) for post_id, similarity in rank.iteritems()] #получаем список достижение-коэффициент насколько подходит для всех достижений
-    recommendations.sort(reverse=True) # Сортируем полученный список
-    recommendation_topM = recommendations[0] # Достаём одно необходимое рекомендованное достижение
+        for post_id in other_user_achievements:  # для id достижения пользователя из other_preferences
+            if post_id not in current_user_achievement:  # проверяем достигнуто ли достижение авторизованным пользователем
+                rank.setdefault(post_id,
+                                0)  # создаём пустую запись для достижения в ранк, если она не была создана раньше
+                rank[post_id] += similarity  # прибавляем коэффициент сходства для достижения
+    recommendations = [(similarity / occurs_in(post_id, preferences, similar_users), post_id) for post_id, similarity in
+                       rank.items()]  # получаем список достижение-коэффициент насколько подходит для всех достижений
+    recommendations.sort(reverse=True)  # Сортируем полученный список
+    recommendation_topM = recommendations[0]  # Достаём одно необходимое рекомендованное достижение
     return (recommendation_topM)
+
+
+# def jaccard_index2():
+#     '''
+# set1 = A B C D E F
+# set2 = A B D F G N
+#
+# пересечение = A B D F
+# объединение = A B C D E F G N
+#     '''
+#     set1 = set(['A', 'B', 'C', 'D', 'E', 'F'])
+#     set2 = set(['A', 'B', 'D', 'F', 'N', 'G'])
+#     intrsct = float(len(set1.intersection(set2)))
+#     print('intrsct', intrsct)
+#     union = float(len(set1.union(set2)))
+#     print('union', union)
+#     jaccard_index = intrsct / union
+#     print('jaccard_index', jaccard_index)
+#     return round(jaccard_index, 3)
+
 
 if __name__ == "__main__":
     user = User.objects.get(id=8)
